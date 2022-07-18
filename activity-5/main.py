b28e595c0f3014446f80f6f7f7d54818752c018c
natan <natanlsr0@gmail.com>
2022-07-18 01:17:21 -0300

    adding activities

--- activity-5/main.py
+++ activity-5/main.py
@@ -1,45 +1,41 @@
 import networkx as nx
 from matplotlib import pyplot as plt
-from math import e, sqrt, pi
 
 
-def degree_distribution(degree_freq, number_nodes):
-    degrees_distribution = [0] * len(degree_freq)
-
-    for i in range(len(degree_freq)):
-        degrees_distribution[i] = degree_freq[i] / number_nodes
-
-    return degrees_distribution
+def format_value(value):
+    return round(value, 3)
 
 
-def get_average_degree(graph):
-    return 2 * graph.number_of_edges() / graph.number_of_nodes()
+def count_float_value(value, values):
+    limited_value = format_value(value)
+    count = 0
+    for value_in_list in values:
+        limited_value_in_list = format_value(value_in_list)
+        if limited_value == limited_value_in_list:
+            count += 1
+    return count
 
 
-def get_poisson_values(k, nodes_degrees):
+def get_frequencies(values):
+    frequencies = {}
+    total = len(values)
+    values_float = [float(x) for x in list(values.values())]
+    for value in values_float:
+        count = format_value(count_float_value(value, values_float) / total)
+        if count != 0.000:
+            frequencies[format_value(value)] = count
+    return frequencies
 
-    qtd_values = len(nodes_degrees)
-    poisson = [0] * qtd_values
-    for degree in range(1, qtd_values):
-        poisson[degree] = ((e ** -k) / sqrt(2*pi*degree)) * (((e * k) / degree) ** degree)
 
-    return poisson
-
-
-def plot_degree_dist(G, name):
-    k = get_average_degree(G)
-    degree_freq = nx.degree_histogram(G)
-    degrees = range(len(degree_freq))
-    degree_distribution_values = degree_distribution(degree_freq, G.number_of_edges())
-    poisson_values = get_poisson_values(k, degrees)
-
-    plt.figure(figsize=(12, 8))
-    plt.loglog(degrees, degree_distribution_values, 'o', label=name)
-    plt.plot(degrees, poisson_values, label="Poisson")
-    plt.ylim([10**-6, 1])
-    plt.legend()
-    plt.xlabel('k')
-    plt.ylabel('Pk')
+def plot_histogram(values):
+    # plt.figure(figsize=(12, 8))
+    # plt.bar(*zip(*values.items()))
+    # plt.hist(values, density=True, bins=80)
+    # plt.ylabel('Y')
+    # plt.xlabel('X')
+    print(values)
+    plt.bar((list(values.keys())), list(values.values()), align='center')
+    plt.show()
 
 
 def read_graph_from_file_and_add_nodes(file_name, graph):
@@ -52,20 +48,32 @@ def read_graph_from_file_and_add_nodes(file_name, graph):
 
 
 def main():
-    graphs_names = ["internet", "collaboration", "protein", "collaboration",
-                    "email", "metabolic", "phonecalls", "powergrid", "www", "citation", "actor"]
+    graphs_names = ["internet", "collaboration", "protein", "email",
+                    "metabolic", "phonecalls", "powergrid", "www", "citation", "actor"]
+    selected_networks = ["protein", "powergrid"]
+
     for graph_name in graphs_names:
         graph = nx.Graph()
-        read_graph_from_file_and_add_nodes("../resources/" + graph_name + ".edgelist.txt", graph)
+        read_graph_from_file_and_add_nodes("./resources/" + graph_name + ".edgelist.txt", graph)
 
         print("Number of nodes from network", graph_name, ": ", graph.number_of_nodes())
-        print("Degree Assortativity Coefficient from network", graph_name, ": ",
-              nx.degree_assortativity_coefficient(graph))
-        print("Degree Pearson Correlation Coefficient", graph_name, ": ",
-              nx.degree_pearson_correlation_coefficient(graph))
-        print("-----------------------------------------------------------------")
-        print()
-        #plot_degree_dist(graph, graph_name)
+
+        if graph_name in selected_networks:
+            # shortest_paths = nx.shortest_path(graph)
+            betweenness_centrality = nx.betweenness_centrality(graph)
+            eigenvector_centrality = nx.eigenvector_centrality(graph)
+            # print(shortest_paths)
+            plot_histogram(get_frequencies(betweenness_centrality))
+            plot_histogram(get_frequencies(eigenvector_centrality))
+
+        # print("Degree Assortativity Coefficient from network", graph_name, ": ",
+        #       nx.degree_assortativity_coefficient(graph))
+        # print("Degree Pearson Correlation Coefficient", graph_name, ": ",
+        #       nx.degree_pearson_correlation_coefficient(graph))
+        # print("-----------------------------------------------------------------")
+        # print()
+
+        # plot_degree_dist(graph, graph_name)
 
 
 if __name__ == '__main__':
--- activity-6/main.py
+++ activity-6/main.py
@@ -1,5 +1,88 @@
+import networkx as nx
+import networkx.algorithms.community as nxcom
+from matplotlib import pyplot as plt
+
+
+def set_node_community(G, communities):
+    """Add community to node attributes"""
+    for c, v_c in enumerate(communities):
+        for v in v_c:
+            # Add 1 to save 0 for external edges
+            G.nodes[v]['community'] = c + 1
+
+
+def set_edge_community(G):
+    """Find internal edges and add their community to their attributes"""
+    for v, w, in G.edges:
+        if G.nodes[v]['community'] == G.nodes[w]['community']:
+            # Internal edge, mark with community
+            G.edges[v, w]['community'] = G.nodes[v]['community']
+        else:
+            # External edge, mark as 0
+            G.edges[v, w]['community'] = 0
+
+
+def get_color(i, r_off=1, g_off=1, b_off=1):
+    """Assign a color to a vertex."""
+    r0, g0, b0 = 0, 0, 0
+    n = 16
+    low, high = 0.1, 0.9
+    span = high - low
+    r = low + span * (((i + r_off) * 3) % n) / (n - 1)
+    g = low + span * (((i + g_off) * 5) % n) / (n - 1)
+    b = low + span * (((i + b_off) * 7) % n) / (n - 1)
+    return r, g, b
+
+
+def read_graph_from_file_and_add_nodes(file_name, graph):
+    with open(file_name) as f:
+        for line in f:
+            number_1, number_2 = line.replace('\n', "").split("\t")
+            i = int(number_1)
+            j = int(number_2)
+            graph.add_edge(i, j)
+
+
 def main():
-    print("main")
+    graphs_names = ["collaboration", "protein"]
+
+    for graph_name in graphs_names:
+        graph = nx.Graph()
+        read_graph_from_file_and_add_nodes("./resources/" + graph_name + ".edgelist.txt", graph)
+
+        print("Number of nodes from network", graph_name, ": ", graph.number_of_nodes())
+        plt.rcParams.update(plt.rcParamsDefault)
+        plt.rcParams.update({'figure.figsize': (15, 10)})
+        plt.style.use('dark_background')
+        communities = sorted(nxcom.greedy_modularity_communities(graph), key=len, reverse=True)
+        # Set node and edge communities
+        set_node_community(graph, communities)
+        set_edge_community(graph)
+        # Set community color for internal edges
+        external = [(v, w) for v, w in graph.edges if graph.edges[v, w]['community'] == 0]
+        internal = [(v, w) for v, w in graph.edges if graph.edges[v, w]['community'] > 0]
+        internal_color = ["black" for e in internal]
+        node_color = [get_color(graph.nodes[v]['community']) for v in graph.nodes]
+        # external edges
+        pos = nx.spring_layout(graph, k=0.1)
+        nx.draw_networkx(
+            graph,
+            pos=pos,
+            node_size=0,
+            edgelist=external,
+            edge_color="silver",
+            node_color=node_color,
+            alpha=0.2,
+            with_labels=False)
+        # internal edges
+        nx.draw_networkx(
+            graph,
+            pos=pos,
+            edgelist=internal,
+            edge_color=internal_color,
+            node_color=node_color,
+            alpha=0.05,
+            with_labels=False)
 
 
 if __name__ == '__main__':
